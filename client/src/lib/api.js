d8a1419da1f47325a914c8ee3508b7bcced02fa1
mlybollinger <mlybollinger@gmail.com>
2026-05-23 16:36:08 -0400

    fix again

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken, clearToken } from './auth';
 
-const api_url = process.env.VITE_API_URL || "http://localhost:3001/api"
+const api_url = process.env.REACT_APP_API_URL || "http://localhost:3001/api"
 
 const api = axios.create({
   baseURL: api_url,
